71a23906e5fd8eab2a363497f255774e6887c90d
Priya Gadhave <priyagadhave20@gmail.com>
2022-03-18 23:00:30 +0530

    First commit

---
+++ eclipse-workspace/Day19_Assignment/src/bridgelabz19/UC2_ValidateUserLastName.java
@@ -0,0 +1,45 @@
+package bridgelabz19;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UC2_ValidateUserLastName {
+	//Declaring patterns
+    static String firstNameRegexPattern = "^[A-Z]{1}[a-z]{3,}$";
+    static String lastNameRegexPattern= "^[A-Z]{1}[a-z]{3,}$";
+
+    //  UC1- method to validate first name
+    public static void validateFirstName() {
+        System.out.print("Enter First Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String fistName = sc.nextLine();
+        Pattern pattern = Pattern.compile(firstNameRegexPattern);
+        Matcher matcher = pattern.matcher(fistName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("FirstName is Valid");
+        } else {
+            System.out.println("FirstName is Invalid");
+        }
+    }
+
+    //  UC2- method to validate last name
+    public static void validateLastName() {
+        System.out.print("Enter Last Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String lastName = sc.nextLine();
+        Pattern pattern = Pattern.compile(lastNameRegexPattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("LastName is Valid");
+        } else {
+            System.out.println("LastName is Invalid");
+        }
+    }
+
+        public static void main(String args[]) {
+            System.out.println("Welcome to User Registration System Program");
+            validateFirstName();
+            validateLastName();
+    }
+ }
---
+++ eclipse-workspace/Day19_Assignment/src/bridgelabz19/UC4_ValidateMobileNumber.java
@@ -0,0 +1,82 @@
+package bridgelabz19;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UC4_ValidateMobileNumber {
+	//Declaring patterns
+    static String firstNameRegexPattern = "^[A-Z]{1}[a-z]{3,}$";
+    static String lastNameRegexPattern= "^[A-Z]{1}[a-z]{3,}$";
+    //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    static String emailRegexPattern = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    static String mobileNoFormatRegexPattern = "^[9,1]{2}?[0-9]{10}$";
+
+
+
+    //  UC1- method to validate first name
+    public static void validateFirstName() {
+        System.out.print("Enter First Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String fistName = sc.nextLine();
+        Pattern pattern = Pattern.compile(firstNameRegexPattern);
+        Matcher matcher = pattern.matcher(fistName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("FirstName is Valid");
+        } else {
+            System.out.println("FirstName is Invalid");
+        }
+    }
+
+    //  UC2- method to validate last name
+    public static void validateLastName() {
+        System.out.print("Enter Last Name :- ");
+        Scanner sc = new Scanner(System.in);
+        String lastName = sc.nextLine();
+        Pattern pattern = Pattern.compile(lastNameRegexPattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("LastName is Valid");
+        } else {
+            System.out.println("LastName is Invalid");
+        }
+    }
+
+    //  UC3- method to validate mail
+    public static void validateMail() {
+        System.out.print("Enter your Mail ID :- ");
+        Scanner sc = new Scanner(System.in);
+        String eMail = sc.nextLine();
+        Pattern pattern = Pattern.compile(lastNameRegexPattern);
+        Matcher matcher = pattern.matcher(eMail);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Mail ID is Valid");
+        } else {
+            System.out.println("Mail ID is Invalid");
+        }
+    }
+
+    //  UC4- method to validate mobile number
+    public static void validateMobileNumber() {
+        System.out.print("Enter Mobile Number :- ");
+        Scanner sc = new Scanner(System.in);
+        String mobNum = sc.next();
+        Pattern pattern = Pattern.compile(mobileNoFormatRegexPattern);
+        Matcher matcher = pattern.matcher(mobNum);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Mobile Number is Valid");
+        } else {
+            System.out.println("Mobile Number is Invalid");
+        }
+    }
+
+        public static void main(String args[]) {
+            System.out.println("Welcome to User Registration System Program");
+            validateFirstName();
+            validateLastName();
+            validateMail();
+            validateMobileNumber();
+    }
+ }
